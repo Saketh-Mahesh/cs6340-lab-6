cc6aa1a6089ce1a80cdbe52b0aa3d782641cee31
Saketh-Mahesh <81051119+Saketh-Mahesh@users.noreply.github.com>
2026-04-16 22:06:05 +0000

    starting off

--- delta/DeltaDebug.java
+++ delta/DeltaDebug.java
@@ -12,16 +12,16 @@
  * or edited. Removing it will be considered an academic integrity issue.
  *
  * We do grant permission to share solutions privately with non-students such
- * as potential employers as long as this header remains in full. However, 
+ * as potential employers as long as this header remains in full. However,
  * sharing with other current or future students or using a medium to share
- * where the code is widely available on the internet is prohibited and 
+ * where the code is widely available on the internet is prohibited and
  * subject to being investigated as a GT honor code violation.
- * Please respect the intellectual ownership of the course materials 
- * (including exam keys, project requirements, etc.) and do not distribute them 
- * to anyone not enrolled in the class. Use of any previous semester course 
- * materials, such as tests, quizzes, homework, projects, videos, and any other 
+ * Please respect the intellectual ownership of the course materials
+ * (including exam keys, project requirements, etc.) and do not distribute them
+ * to anyone not enrolled in the class. Use of any previous semester course
+ * materials, such as tests, quizzes, homework, projects, videos, and any other
  * coursework, is prohibited in this course. */
- 
+
 import java.io.BufferedReader;
 import java.io.File;
 import java.io.FileNotFoundException;
@@ -36,72 +36,104 @@ import java.util.Scanner;
 import java.util.List;
 
 public class DeltaDebug {
-	
+
 	/**
 	 * deltaDebug is the method is what will run the delta debug algorithm
-	 * !!!!!! IMPORTANT: DO NOT CHANGE THE TYPE/METHOD SIGNATURE AS IT MUST BE THE SAME AS PROVIDED FOR GRADING !!!!!!  
+	 * !!!!!! IMPORTANT: DO NOT CHANGE THE TYPE/METHOD SIGNATURE AS IT MUST BE THE SAME AS PROVIDED FOR GRADING !!!!!!
 	 * @param char_granularity - if false, use line granularity for the algorithm
 	 * @param program - the path of the program you're testing, e.g. "./SecretCoder"
 	 * @param failing_file - path of provided failing input file, e.g. "./input_file.txt"
 	 * @param error_msg - the program output that Delta should treat as an error, e.g. "java.lang.ArrayIndexOutOfBoundsException"
-	 * @param final_minimized_file - path to write minimized output file to  
+	 * @param final_minimized_file - path to write minimized output file to
 	 */
 	public void deltaDebug(Boolean char_granularity, String program, String failing_file, String error_msg, String final_minimized_file)
 	{
-        /*
-        TODO: Add your logic for the delta debugging algorithm here
-        */
+		List<String> input = readFile(failing_file, char_granularity);
+		Integer n = 2;
+
+		while (true) {
+			
+		}
+
 	}
-	
+
+	public Boolean testProgram(String program, List<String> input, String error_message) {
+		Boolean wrote_file = writeToFile("temp_input.txt", input);
+		if (!wrote_file) {
+			System.out.println("Failed to write to file");
+			System.exit(-1);
+		}
+
+		String command = program + " temp_input.txt";
+		return runCommand(command, error_message);
+	}
+
 	/**
 	 * readFile reads input from a file line by line
 	 * You can update this method to pass in more parameters or return something if needed
 	 * @param file - file to read
+	 * @param char_granularity - if false, use line granularity for the algorithm
 	 */
-	public void readFile(String file)
+	public List<String> readFile(String file, Boolean char_granularity)
 	{
 		Scanner scan;
+		List<String> fileList = new ArrayList<>();
 		try {
 			scan = new Scanner(new File(file));
 			while (scan.hasNextLine()){
-			    //do something
+				if (char_granularity) {
+					scan.useDelimiter("\\Z");
+					if (scan.hasNext()) {
+						String content = scan.next();
+						fileList.addAll(Arrays.asList(content.split("")));
+					}
+				}
+				else {
+					String line = scan.nextLine();
+					fileList.add(line);
+				}
 			}
 			scan.close();
 		} catch (FileNotFoundException e) {
 			e.printStackTrace();
 		}
+
+		return fileList;
 	}
-	
+
 	/**
-	 * runCommand can be used to run a program 
+	 * runCommand can be used to run a program
 	 * You can update this method to pass in more/different parameters or return something if needed
 	 * @param command - complete command you want to run (program location + any command args)
 	 * @param error - error message you're looking for
 	 */
-	public void runCommand(String command, String error_msg)
-	{
-		String s = null;
-		try{ 	
-            Process p = Runtime.getRuntime().exec(command);
-            
-            BufferedReader stdInput = new BufferedReader(new 
-                 InputStreamReader(p.getErrorStream()));
-            // read the output from the command
-            while ((s = stdInput.readLine()) != null) {
-                //do something
-            }
+	public boolean runCommand(String command, String error_msg) {
+		try {
+			Process p = Runtime.getRuntime().exec(command);
+			BufferedReader stdInput = new BufferedReader(
+				new InputStreamReader(p.getErrorStream()));
+
+			boolean found = false;
+			String s;
+			while ((s = stdInput.readLine()) != null) {
+				if (s.contains(error_msg)) {
+					found = true;
+				}
+			}
+			p.waitFor();
+			return found;
+		} catch (IOException e) {
+			System.out.println(command + "failed to run");
+			System.exit(-1);
+			return false;
 		}
-		catch (IOException e) {
-            System.out.println(command + "failed to run");
-            System.exit(-1);
-        }
 	}
 
 	/**
      * writeFile() can be used to write to a file
      * This is a basic method for writing your String to a file
-     * You can use helper methods to call on this method if you wish, but we do not recommend modifying 
-     * this method directly. 
+     * You can use helper methods to call on this method if you wish, but we do not recommend modifying
+     * this method directly.
      * @param file - file to write to
      * @param List<String> - abstract data structure for holding String objects, you can pass in an ArrayList<String>, for example
      */
@@ -115,8 +147,8 @@ public class DeltaDebug {
             return false;
         }
         return true;
-    }		
-	
+    }
+
 
 }
 
